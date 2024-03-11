8feab4cf5f2459b99cd5ae069cb65a8e7c010bb1
David Fei-Zhang <david.fei-zhang@northwestern.edu>
2024-03-11 19:01:14 +0000

    jhjhjh jhjhjhjhhjhjhj
    asdfasdfasdf

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,41 @@
 class EntriesController < ApplicationController
 
-  def new
-  end
+ def index
+  @entries - Entry.all
 
-  def create
+  respond_to do |format|
+    format.html # implicitly renders entries/index.html.erb
+    format.json do
+      render :json => @entries
+    end
+  end
+end
+
+ def new
+  @user = User.find_by({ "id" => session["user_id"] })
+end
+
+def create
+  @user = User.find_by({ "id" => session["user_id"] })
+  if @user != nil
     @entry = Entry.new
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["place_id"] = params["place_id"]
     @entry.save
+  else
+    flash["notice"] = "Login first."
+  end 
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
+  
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
 end
